38468b7aed4dc60199252c3576d548e22d5b5aa0
Bruno Moraes <89628890+brunomoraesdigital@users.noreply.github.com>
2023-03-03 23:28:20 -0300

    Adicionando arquivos iniciais

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({ //decorator de classe
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent {
+
+}
